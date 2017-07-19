8f1340e471e7c412e555978f6dfedcb0847f122d
kc-curefit <kunal@curefit.com>
2017-07-20 01:39:01 +0530

    error handling and logging

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var gpio = require('rpi-gpio');
- 
+
 gpio.setup(7, gpio.DIR_OUT, write);
- 
+
 function write() {
-    gpio.write(7, true, function(err) {
-        if (err) throw err;
-        console.log('Written to pin');
-    });
+  console.log('Writting to pin 7');
+  gpio.write(7, true, function (err) {
+    if (err) {
+      console.log(err)
+    }
+    console.log('Written to pin 7');
+  });
 }
