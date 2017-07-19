636037f2cf8bdc00aed309ae98595399584fa447
kc-curefit <kunal@curefit.com>
2017-07-20 02:16:07 +0530

    update

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-var gpio = require('rpi-gpio');
+var gpio = require("gpio");
+var gpio22, gpio4, intervalTimer;
  
-var pin   = 7;
-var delay = 2000;
-var count = 0;
-var max   = 300;
-
-console.log('start');
-
-gpio.setup(pin, gpio.DIR_OUT, on);
+// Flashing lights if LED connected to GPIO22 
+gpio22 = gpio.export(22, {
+   ready: function() {
+      intervalTimer = setInterval(function() {
+         gpio22.set();
+         setTimeout(function() { gpio22.reset(); }, 500);
+      }, 1000);
+   }
+});
  
-function on() {
-    console.log('on');
-    if (count >= max) {
-        gpio.destroy(function() {
-            console.log('Closed pins, now exit');
-        });
-        return;
-    }
+// Lets assume a different LED is hooked up to pin 4, the following code  
+// will make that LED blink inversely with LED from pin 22  
+gpio4 = gpio.export(4, {
+   ready: function() {
+      // bind to gpio22's change event 
+      gpio22.on("change", function(val) {
+         gpio4.set(1 - val); // set gpio4 to the opposite value 
+      });
+   }
+});
  
-    setTimeout(function() {
-        gpio.write(pin, 1, off);
-        count += 1;
-    }, delay);
-}
- 
-function off() {
-    setTimeout(function() {
-        gpio.write(pin, 0, on);
-    }, delay);
-}
+// reset the headers and unexport after 10 seconds 
+setTimeout(function() {
+   clearInterval(intervalTimer);          // stops the voltage cycling 
+   gpio22.removeAllListeners('change');   // unbinds change event 
+   gpio22.reset();                        // sets header to low 
+   gpio22.unexport();                     // unexport the header 
+   
+   gpio4.reset();
+   gpio4.unexport(function() {
+      // unexport takes a callback which gets fired as soon as unexporting is done 
+      process.exit(); // exits your node program 
+   });
+}, 10000)
